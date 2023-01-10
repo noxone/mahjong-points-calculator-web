dbd893c1f0c4fd92d1f26653ec8855a0c710e2b1
neumaol <neumaol@hlag.com>
2023-01-10 16:09:57 +0100

    Some basic interaction

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -4,18 +4,45 @@ import org.w3c.dom.Node
 import kotlinx.browser.document
 import kotlinx.browser.window
 import kotlinx.dom.clear
+import kotlinx.html.DIV
+import kotlinx.html.a
 import kotlinx.html.img
-import org.olafneumann.mahjong.points.model.Color
+import kotlinx.html.js.onClickFunction
 import org.olafneumann.mahjong.points.model.Tile
 
 fun main() {
-    window.onload = { document.getElementById("mr_tiles")?.appendTileDivs() }
+    window.onload = { initMahjongPointCalculator() }
 }
 
-fun Node.sayHello() {
+private fun initMahjongPointCalculator() {
+    document.getElementById("mr_tiles")?.appendTileDivs()
+}
+
+val selectedTiles = mutableListOf<Tile>()
+
+fun DIV.tileImage(tile: Tile) = a {
+    img(classes = "mr-tile", src = "images/${tile.filename}") {
+        alt = tile.name
+    }
+    onClickFunction = {
+        selectedTiles += tile
+        showSelectedNodes()
+
+        if (selectedTiles.filter { it == tile }.size >= 4) {
+            console.log("disable: $tile")
+        }
+    }
+}
+
+fun showSelectedNodes() {
+    document.getElementById("mr_selected_tiles")?.showSelectedTiles(selectedTiles)
+}
+
+fun Node.showSelectedTiles(tiles: Collection<Tile>) {
+    clear()
     append {
         div {
-            +"Hello from JS"
+            tiles.forEach { tileImage(it) }
         }
     }
 }
@@ -25,42 +52,28 @@ fun Node.appendTileDivs() {
     append {
         div(classes = "mr-tile-field") {
             div {
-                Tile.bamboos.forEach {
-                    img(classes = "mr-tile", src = "images/${it.filename}") { }
-                }
+                Tile.bamboos.forEach { tileImage(it) }
             }
             div {
-                Tile.characters.forEach {
-                    img(classes = "mr-tile", src = "images/${it.filename}") { }
-                }
+                Tile.characters.forEach { tileImage(it) }
             }
             div {
-                Tile.cirles.forEach {
-                    img(classes = "mr-tile", src = "images/${it.filename}") { }
+                Tile.cirles.forEach { tileImage(it) }
+            }
+            div(classes = "d-flex justify-content-between") {
+                div {
+                    Tile.winds.forEach { tileImage(it) }
+                }
+                div {
+                    Tile.dragons.forEach { tileImage(it) }
                 }
             }
             div(classes = "d-flex justify-content-between") {
                 div {
-                    Tile.winds.forEach {
-                        img(classes = "mr-tile", src = "images/${it.filename}") { }
-                    }
+                    Tile.flowers.forEach { tileImage(it) }
                 }
                 div {
-                    Tile.dragons.forEach {
-                        img(classes = "mr-tile", src = "images/${it.filename}") { }
-                    }
-                }
-            }
-            div(classes = "d-flex justify-content-between") {
-                div {
-                    Tile.flowers.forEach {
-                        img(classes = "mr-tile", src = "images/${it.filename}") { }
-                    }
-                }
-                div {
-                    Tile.seasons.forEach {
-                        img(classes = "mr-tile", src = "images/${it.filename}") { }
-                    }
+                    Tile.seasons.forEach { tileImage(it) }
                 }
             }
         }
