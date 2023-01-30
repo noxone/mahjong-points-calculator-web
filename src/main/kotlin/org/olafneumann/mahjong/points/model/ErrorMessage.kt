2b46c43955304f6bdbc21c147c6684fb984fbfe9
Olaf Neumann <github@olafneumann.org>
2023-01-30 22:10:09 +0100

    Blink tile if not available

--- src/main/kotlin/org/olafneumann/mahjong/points/model/ErrorMessage.kt
+++ src/main/kotlin/org/olafneumann/mahjong/points/model/ErrorMessage.kt
@@ -3,6 +3,5 @@ package org.olafneumann.mahjong.points.model
 import org.olafneumann.mahjong.points.game.Tile
 
 data class ErrorMessage(
-    val tile: Tile,
-    val message: String,
+    val tile: Tile? = null,
 )
