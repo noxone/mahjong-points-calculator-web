5d95845048907cb7a40e6e3f20146ad3df3d5dae
Olaf Neumann <github@olafneumann.org>
2023-01-31 15:51:21 +0100

    Animate concealing tiles #26

--- src/main/kotlin/org/olafneumann/mahjong/points/ui/components/HandComponent.kt
+++ src/main/kotlin/org/olafneumann/mahjong/points/ui/components/HandComponent.kt
@@ -1,11 +1,9 @@
 package org.olafneumann.mahjong.points.ui.components
 
 import kotlinx.browser.document
-import kotlinx.dom.clear
 import kotlinx.html.ButtonType
 import kotlinx.html.TagConsumer
 import kotlinx.html.button
-import kotlinx.html.dom.append
 import kotlinx.html.js.div
 import kotlinx.html.js.onClickFunction
 import kotlinx.html.span
@@ -14,7 +12,8 @@ import org.olafneumann.mahjong.points.lang.not
 import org.olafneumann.mahjong.points.model.Figure
 import org.olafneumann.mahjong.points.model.getCombination
 import org.olafneumann.mahjong.points.model.getTiles
-import org.olafneumann.mahjong.points.ui.controls.tileImage
+import org.olafneumann.mahjong.points.ui.controls.TileImage
+import org.olafneumann.mahjong.points.ui.controls.TileImage.Companion.createTileImage
 import org.olafneumann.mahjong.points.ui.html.MrAttributes
 import org.olafneumann.mahjong.points.ui.html.filterAttributeIsPresent
 import org.olafneumann.mahjong.points.ui.html.getAllChildren
@@ -38,7 +37,7 @@ class HandComponent(
     val model: UIModel,
 ) : AbstractComponent(parent = parent), UIModelChangeListener {
     private var selectableDivs: Map<Figure, HTMLDivElement> by Delegates.notNull()
-    private var tileContainerDivs: Map<Figure, HTMLDivElement> by Delegates.notNull()
+    private var figureTiles: MutableMap<Figure, MutableList<TileImage>> = mutableMapOf()
     private var figureSwitches: Map<Figure, HTMLInputElement> by Delegates.notNull()
     private var figurePopovers: Map<Figure, Popover> by Delegates.notNull()
     private val btnUndo = document.getElement<HTMLButtonElement>("mr_btn_undo")
@@ -57,14 +56,13 @@ class HandComponent(
 
     override fun TagConsumer<HTMLElement>.createUI() {
         injectRoot { element ->
-            tileContainerDivs = element.getAllChildren<HTMLDivElement>()
+            selectableDivs = element.getAllChildren<HTMLDivElement>()
                 .filterAttributeIsPresent(MrAttributes.FIGURE)
                 .associateBy { Figure.valueOf(it.mrFigure!!) }
-            selectableDivs = tileContainerDivs.mapValues { (_, div) -> div.parentElement!! as HTMLDivElement }
             figureSwitches = element.getAllChildren<HTMLInputElement>()
                 .mapIndexed { index, input -> Figure.values()[index] to input }
                 .toMap()
-            figurePopovers = tileContainerDivs.map { it.key to createPopover(it.value, it.key) }
+            figurePopovers = selectableDivs.map { it.key to createPopover(it.value, it.key) }
                 .toMap()
         }
             .div(classes = "flex-fill mr-figure-list") {
@@ -83,11 +81,15 @@ class HandComponent(
             div(classes = "${isBonus.toString("col", "col-8 col-md-9")} mr-figure border") {
                 span { +!figure.title }
                 div(classes = "mr-tile-container") {
-                    mrFigure = figure.name
-                    onClickFunction = {
-                        it.stopPropagation()
-                        handleFigureClick(figure)
+                    figureTiles[figure] = mutableListOf()
+                    for (i in 1..figure.maxTilesPerFigure) {
+                        figureTiles[figure]!!.add(createTileImage(null))
                     }
+                }
+                mrFigure = figure.name
+                onClickFunction = {
+                    it.stopPropagation()
+                    handleFigureClick(figure)
                 }
             }
             if (figure != Figure.Bonus) {
@@ -128,24 +130,14 @@ class HandComponent(
         selectableDivs.forEach { (figure, div) ->
             div.classList.toggle("mr-selected", figure == model.calculatorModel.selectedFigure)
         }
-        tileContainerDivs.forEach { (figure, div) ->
-            div.clear()
-            div.append {
-                val combination = model.calculatorModel.hand.getCombination(figure)
-                val isConcealed = combination?.let { it.visibility == Combination.Visibility.Closed } ?: false
-                model.calculatorModel.hand.getTiles(figure)
-                    .sortedBy { it.ordinal }
-                    .forEachIndexed { index, tile ->
-                        tileImage(
-                            tile,
-                            backside = isConcealed && (index == 1 || (index == 2 && combination?.type == Combination.Type.Kang)),
-                            selectable = true
-                        )
-                    }
-                model.calculatorModel.hand
-                    .getTiles(figure)
-                    .sortedBy { it.ordinal }
-
+        Figure.values().forEach { figure ->
+            val combination = model.calculatorModel.hand.getCombination(figure)
+            val isConcealed = combination?.let { it.visibility == Combination.Visibility.Closed } ?: false
+            figureTiles[figure]!!.forEach { image -> image.tile = null }
+            model.calculatorModel.hand.getTiles(figure).sortedBy { it.ordinal }.forEachIndexed { index, tile ->
+                val tileImage = figureTiles[figure]!![index]
+                tileImage.tile = tile
+                tileImage.backside = isConcealed && (index == 1 || (index == 2 && combination?.type == Combination.Type.Kang))
             }
         }
         figureSwitches.forEach { (figure, input) ->
--- src/main/kotlin/org/olafneumann/mahjong/points/ui/controls/TileImage.kt
+++ src/main/kotlin/org/olafneumann/mahjong/points/ui/controls/TileImage.kt
@@ -1,16 +1,15 @@
 package org.olafneumann.mahjong.points.ui.controls
 
+import kotlinx.browser.window
 import kotlinx.html.TagConsumer
-import kotlinx.html.a
-import kotlinx.html.id
-import kotlinx.html.img
 import kotlinx.html.js.div
 import kotlinx.html.js.onClickFunction
 import org.olafneumann.mahjong.points.game.Tile
+import org.olafneumann.mahjong.points.ui.html.injectRoot
 import org.olafneumann.mahjong.points.ui.html.mrTile
 import org.olafneumann.mahjong.points.util.toString
+import org.w3c.dom.HTMLDivElement
 import org.w3c.dom.HTMLElement
-import org.w3c.dom.HTMLImageElement
 import org.w3c.dom.events.Event
 
 fun TagConsumer<HTMLElement>.tileImage(
@@ -19,7 +18,13 @@ fun TagConsumer<HTMLElement>.tileImage(
     selectable: Boolean = true,
     onClickFunction: (Event) -> Unit = {}
 ) =
-    div(classes = "mr-tile ${backside.toString("mr-tile-backside")} ${(tile != null).toString("mr-tile-background")} ${(!selectable).toString("not-selectable")}") {
+    div(
+        classes = "mr-tile ${backside.toString("mr-tile-backside")} ${(tile != null).toString("mr-tile-background")} ${
+            (!selectable).toString(
+                "not-selectable"
+            )
+        }"
+    ) {
         div(classes = tile.cssClass) {
             if (tile != null) {
                 mrTile = tile.name
@@ -28,7 +33,71 @@ fun TagConsumer<HTMLElement>.tileImage(
         this.onClickFunction = onClickFunction
     }
 
-private val Tile?.cssClass: String get() = this?.let { "mr-tile-content mr-tile-${name.lowercase()}" } ?: ""
+private val Tile?.cssClass: String get() = this?.let { "mr-tile-face" } ?: ""
 
 fun HTMLElement.setSelectable(selectable: Boolean) =
     parentElement!!.classList.toggle("not-selectable", !selectable)
+
+class TileImage private constructor(
+    private val outer: HTMLDivElement,
+    private val inner: HTMLDivElement,
+    tile: Tile?
+) {
+    var tile: Tile? = null
+        set(value) {
+            field = value
+            setTileName(tile?.name)
+            activateBackground(tile != null)
+            showTile(tile != null)
+            if (value == null) {
+                backside = false
+            }
+        }
+
+    var backside: Boolean
+        get() = outer.classList.contains(CLS_BACKSIDE)
+        set(value) {
+            outer.classList.toggle(CLS_ROTATED, value)
+            window.setTimeout({ outer.classList.toggle(CLS_BACKSIDE, value) }, 250)
+        }
+
+    var selectable: Boolean
+        get() = !outer.classList.contains(CLS_NOT_SELECTABLE)
+        set(value) {
+            outer.classList.toggle(CLS_NOT_SELECTABLE, !value)
+        }
+
+    private fun activateBackground(enabled: Boolean) =
+        outer.classList.toggle(CLS_BACKGROUND, enabled)
+
+    private fun showTile(show: Boolean) =
+        outer.classList.toggle("d-none", !show)
+
+    private fun setTileName(name: String?) {
+        inner.mrTile = name ?: ""
+    }
+
+    init {
+        this.tile = tile
+    }
+
+    companion object {
+        private const val CLS_BACKSIDE = "mr-tile-backside"
+        private const val CLS_ROTATED = "mr-tile-rotated"
+        private const val CLS_BACKGROUND = "mr-tile-background"
+        private const val CLS_NOT_SELECTABLE = "not-selectable"
+
+        fun TagConsumer<HTMLElement>.createTileImage(tile: Tile?): TileImage {
+            var outer: HTMLDivElement? = null
+            var inner: HTMLDivElement? = null
+            injectRoot { outer = it as HTMLDivElement }
+                .div(classes = "mr-tile") {
+                    injectRoot { inner = it as HTMLDivElement }
+                        .div(classes = "mr-tile-face") {
+                        }
+                    // this.onClickFunction = onClickFunction
+                }
+            return TileImage(outer = outer!!, inner = inner!!, tile = tile)
+        }
+    }
+}
