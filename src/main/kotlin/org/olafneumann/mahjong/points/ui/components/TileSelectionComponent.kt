f149ddfdf9e8ba523a1a6be04004a04a722a900b
Olaf Neumann <github@olafneumann.org>
2023-02-01 08:13:22 +0100

    Improve tile selection

--- src/main/kotlin/org/olafneumann/mahjong/points/ui/components/TileSelectionComponent.kt
+++ src/main/kotlin/org/olafneumann/mahjong/points/ui/components/TileSelectionComponent.kt
@@ -3,58 +3,48 @@ package org.olafneumann.mahjong.points.ui.components
 import kotlinx.browser.window
 import kotlinx.html.TagConsumer
 import kotlinx.html.div
-import org.olafneumann.mahjong.points.ui.html.getAllChildren
-import org.olafneumann.mahjong.points.ui.html.filterAttributeIsPresent
-import org.olafneumann.mahjong.points.ui.html.injectRoot
 import org.olafneumann.mahjong.points.game.Tile
-import org.olafneumann.mahjong.points.ui.controls.setSelectable
-import org.olafneumann.mahjong.points.ui.controls.tileImage
-import org.olafneumann.mahjong.points.ui.html.MrAttributes
-import org.olafneumann.mahjong.points.ui.html.mrTile
+import org.olafneumann.mahjong.points.ui.controls.TileImage
+import org.olafneumann.mahjong.points.ui.controls.TileImage.Companion.createTileImage
 import org.olafneumann.mahjong.points.ui.model.UIModel
 import org.olafneumann.mahjong.points.ui.model.UIModelChangeListener
 import org.w3c.dom.HTMLElement
-import org.w3c.dom.HTMLImageElement
 import org.w3c.dom.events.Event
-import org.w3c.dom.get
 import kotlin.properties.Delegates
 
 class TileSelectionComponent(
     parent: HTMLElement,
     private val model: UIModel
 ) : AbstractComponent(parent = parent), UIModelChangeListener {
-    private var imageTiles: Map<Tile, HTMLElement> by Delegates.notNull()
+    private var imageTiles: Map<Tile, TileImage> by Delegates.notNull()
 
     init {
         model.registerChangeListener(this)
     }
 
     override fun TagConsumer<HTMLElement>.createUI() {
-        injectRoot { element ->
-            imageTiles = element.getAllChildren<HTMLElement>()
-                .filterAttributeIsPresent(MrAttributes.TILE)
-                .associateBy { Tile.valueOf(it.mrTile!!) }
-        }
-            .div(classes = "mr-tile-field mr-tile-container d-flex flex-column justify-content-between flex-fill") {
-                div { tileImages(Tile.bamboos) }
-                div { tileImages(Tile.characters) }
-                div { tileImages(Tile.circles) }
-                div {
-                    tileImages(Tile.winds)
-                    tileImage()
-                    tileImage()
-                    tileImages(Tile.dragons)
-                }
-                div {
-                    tileImages(Tile.flowers)
-                    tileImage()
-                    tileImages(Tile.seasons)
-                }
+        val imageTiles = mutableMapOf<Tile, TileImage>()
+        div(classes = "mr-tile-field mr-tile-container d-flex flex-column justify-content-between flex-fill") {
+            div { imageTiles.putAll(tileImages(Tile.bamboos)) }
+            div { imageTiles.putAll(tileImages(Tile.characters)) }
+            div { imageTiles.putAll(tileImages(Tile.circles)) }
+            div {
+                imageTiles.putAll(tileImages(Tile.winds))
+                createTileImage(null)
+                createTileImage(null)
+                imageTiles.putAll(tileImages(Tile.dragons))
             }
+            div {
+                imageTiles.putAll(tileImages(Tile.flowers))
+                createTileImage(null)
+                imageTiles.putAll(tileImages(Tile.seasons))
+            }
+        }
+        this@TileSelectionComponent.imageTiles = imageTiles
     }
 
-    private fun TagConsumer<HTMLElement>.tileImages(tiles: Collection<Tile>) =
-        tiles.forEach { tile -> tileImage(tile, selectable = tile.isSelectable, onClickFunction = createOnClickListener(tile)) }
+    private fun TagConsumer<HTMLElement>.tileImages(tiles: Collection<Tile>): Map<Tile, TileImage> =
+        tiles.associateWith { createTileImage(tile = it, onClickFunction = createOnClickListener(it)) }
 
     private fun createOnClickListener(tile: Tile): (Event) -> Unit = {
         if (tile.isSelectable) {
@@ -64,21 +54,12 @@ class TileSelectionComponent(
 
     override fun updateUI() {
         imageTiles.forEach { (tile, element) ->
-            element.setSelectable(tile.isSelectable)
+            element.selectable = tile.isSelectable
         }
 
         model.calculatorModel.errorMessages.forEach { errorMessage ->
-            errorMessage.tile?.let { showAlertFor(it) }
+            errorMessage.tile?.let { imageTiles[it]?.blinkForAlert() }
         }
-    }
-
-    private fun showAlertFor(tile: Tile) {
-        toggleAlertFor(tile, true)
-        window.setTimeout({ toggleAlertFor(tile, false) }, 1000)
-    }
-
-    private fun toggleAlertFor(tile: Tile, enabled: Boolean) {
-        imageTiles[tile]!!.parentElement!!.classList.toggle("mr-alert", enabled)
     }
 
     override fun modelChanged(model: UIModel) {
