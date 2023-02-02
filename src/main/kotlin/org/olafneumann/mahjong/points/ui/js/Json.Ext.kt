74abfc13dcf079fab5f34d217af24be3e9179f37
Olaf Neumann <github@olafneumann.org>
2023-02-02 11:52:52 +0100

    Remove spread operator

--- src/main/kotlin/org/olafneumann/mahjong/points/ui/js/Json.Ext.kt
+++ src/main/kotlin/org/olafneumann/mahjong/points/ui/js/Json.Ext.kt
@@ -3,8 +3,7 @@ package org.olafneumann.mahjong.points.ui.js
 import kotlin.js.Json
 import kotlin.js.json
 
-fun Map<String,Any?>.toJson(): Json = json(
-    *this.filter { it.value != null }
-        .map { it.key to it.value }
-        .toTypedArray()
-)
+fun Map<String, Any?>.toJson(): Json = json(pairs = this
+    .filter { it.value != null }
+    .map { it.key to it.value }
+    .toTypedArray())
